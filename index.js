8bc6bcd64825f410057623e82602f8ab8fbb70f0
Erick Acosta <erick26acosta@outlook.com>
2023-10-24 13:09:58 -0300

    Conflito de merge resolvido

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import { turboLogger } from "./turboLogger.js";
 console.log("Estou usando o Github");
 turboLogger("Estou aprendendo o fluxo do git");
 turboLogger("Estou aprendendo o fluxo do git", "red");
+turboLogger(
+  "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema!",
+  "darkgreen"
+);
